b660d05bbf48bbaeb750bc402cdda8adafd31694
Glenn Quintyn <glenn.quintyn@gmail.com>
2023-03-01 14:17:29 +0100

    Revert "Added HandInputManager"
    This reverts commit 81e083bf8c961518743dd4bcaeb8491d8b038b67.

--- Assets/Grapple2.0/Scripts/HandInputManager.cs
+++ Assets/Grapple2.0/Scripts/HandInputManager.cs
@@ -1,32 +1,18 @@
-using Rhinox.Grappler.BoneManagement;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-
-namespace Rhinox.XR.Grapple
+public class HandInputManager : MonoBehaviour
 {
-    public class HandInputManager : MonoBehaviour
+    // Start is called before the first frame update
+    void Start()
     {
-        private BoneManager _boneManager = null;
+        
+    }
 
-        private IPhysicsService _physicsService = null;
-
-        void Start()
-        {
-            _boneManager = gameObject.AddComponent<BoneManager>();
-            if (_boneManager == null)
-            {
-                Debug.LogError($"{nameof(HandInputManager)} Failled to add {nameof(BoneManager)}");
-            }
-
-
-        }
-
-        // Update is called once per frame
-        void Update()
-        {
-
-        }
+    // Update is called once per frame
+    void Update()
+    {
+        
     }
 }
