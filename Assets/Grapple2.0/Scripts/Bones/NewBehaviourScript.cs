bd5ba465578dd11a23b4152341b4a1b89a9ecee5
Glenn Quintyn <glenn.quintyn@gmail.com>
2023-03-01 08:58:02 +0100

    Added Bone Services
    Added base bone interface and a OculusBoneService

---
+++ Assets/Grapple2.0/Scripts/Bones/NewBehaviourScript.cs
@@ -0,0 +1,21 @@
+using Rhinox.XR.Grapple;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //var idk = new OculusBoneService();
+        //idk.Initialize(null);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
