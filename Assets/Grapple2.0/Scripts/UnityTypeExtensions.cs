867720b3d7abce485e4116f07511e6dc2be41038
Tristan Wauthier <tristan.wauthier@hotmail.com>
2023-03-06 11:13:52 +0100

    [Fix] General bugfixing

---
+++ Assets/Grapple2.0/Scripts/UnityTypeExtensions.cs
@@ -0,0 +1,8 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class UnityTypeExtensions : MonoBehaviour
+{
+    public static bool Approximately(Quaternion q1, Quaternion q2, float )
+}
