2f65b6f353a50fd15c708db00f95076f5268a49b
Glenn Quintyn <glenn.quintyn@gmail.com>
2023-04-11 17:37:34 +0200

    [MOD] Valve almost working
    Valve can rotate when a hand grabs it, but the rotation value is incorrectly calculated.

--- Assets/GrappleIT/Scripts/Interactables/Valve/GRPLValve.cs
+++ Assets/GrappleIT/Scripts/Interactables/Valve/GRPLValve.cs
@@ -1,6 +1,7 @@
-using log4net.Util;
+//using log4net.Util;
 using Rhinox.GUIUtils.Editor;
 using Rhinox.Lightspeed;
+using Rhinox.Perceptor;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,6 +12,8 @@ namespace Rhinox.XR.Grapple.It
 {
     public class GRPLValve : GRPLInteractable
     {
+        [SerializeField] private Transform _visualTransform = null;
+
         [Header("Grab parameters")]
         [SerializeField] private string _grabGestureName = "Grab";
         [SerializeField] private float _grabRadius = .1f;
@@ -24,24 +27,26 @@ namespace Rhinox.XR.Grapple.It
         [SerializeField] bool _drawGizmos = true;
 
         public bool IsGrabbed { get; protected set; } = false;
-        public float CurrentValveRotation => _valveRotation;
+        public float CurrentValveRotation => _currentValveRotation;
 
         //righty tighty, lefty loosey
         public Action<GRPLValve> OnFullyClosed;
         public Action<GRPLValve> OnFullyOpen;
-        public Action<GRPLValve, float> OnValueChanged;
+        public Action<GRPLValve, float> OnValueUpdate;
 
 
-        private float _minRadius;
-        private float _maxRadius;
-        private float _valveRotation = 180f;
+        private float _minRadius = 0f;
+        private float _maxRadius = 0f;
+        private float _currentValveRotation = 0f;
 
-        private RhinoxGesture _grabGesture;
-        private static GRPLJointManager _jointManager;
-        private bool _canHandGrabL;
-        private bool _canHandGrabR;
-        private RhinoxHand _currentHandHolding;
-        private RhinoxJoint _interactingJoint;
+        private RhinoxGesture _grabGesture = null;
+        private static GRPLJointManager _jointManager = null;
+        private bool _canHandGrabL = false;
+        private bool _canHandGrabR = false;
+        private RhinoxHand _currentHandHolding = RhinoxHand.Invalid;
+        private RhinoxJoint _interactingJoint = null;
+
+        private Vector3 _grabbedVec = Vector3.positiveInfinity;
 
         protected void Awake()
         {
@@ -53,9 +58,23 @@ namespace Rhinox.XR.Grapple.It
         {
             base.Initialize();
 
+            if (_visualTransform == null)
+            {
+                PLog.Error<GRPLITLogger>($"[GRPLValve:Initialize], " +
+                    $"Visual Transform was null", this);
+                return;
+            }
+
+            float currentRot = _visualTransform.localEulerAngles.z;
+            if (currentRot > _fullyClosedAngle)
+                _visualTransform.localEulerAngles.With(null, null, _fullyClosedAngle);
+            else if (currentRot < _fullyOpenAngle)
+                _visualTransform.localEulerAngles.With(null, null, _fullyOpenAngle);
+
+            _currentValveRotation = _visualTransform.localEulerAngles.z;
+
             _minRadius = _grabRadius - _grabTolaranceRadius;
             _maxRadius = _grabRadius + _grabTolaranceRadius;
-            GRPLGestureRecognizer.GlobalInitialized += GestureRecognizerInitialized;
         }
 
         private void JointManagerInitialized(GRPLJointManager jointManager)
@@ -151,41 +170,6 @@ namespace Rhinox.XR.Grapple.It
                     break;
             }
 
-            //var projected = Vector3.ProjectOnPlane(joint.JointPosition, -transform.forward);
-            //var smtsmt = transform.position - projected;
-            //TOOD: CLEAN THIS UP
-            //if (dst >= _minRadius * _minRadius && dst <= _maxRadius * _maxRadius)
-            //{
-            //    switch (hand)
-            //    {
-            //        case RhinoxHand.Left:
-            //            _canHandGrabL = true;
-            //            break;
-            //        case RhinoxHand.Right:
-            //            _canHandGrabR = true;
-            //            break;
-            //        case RhinoxHand.Invalid:
-            //        default:
-            //            break;
-            //    }
-            //    //Debug.Log("GOOD");
-            //}
-            //else
-            //{
-            //    switch (hand)
-            //    {
-            //        case RhinoxHand.Left:
-            //            _canHandGrabL = false;
-            //            break;
-            //        case RhinoxHand.Right:
-            //            _canHandGrabR = false;
-            //            break;
-            //        case RhinoxHand.Invalid:
-            //        default:
-            //            break;
-            //    }
-            //}
-
             return IsGrabbed;
         }
 
@@ -205,6 +189,7 @@ namespace Rhinox.XR.Grapple.It
                 return;
 
             IsGrabbed = true;
+            _grabbedVec = _interactingJoint.JointPosition;
             _currentHandHolding = hand;
         }
 
@@ -213,15 +198,42 @@ namespace Rhinox.XR.Grapple.It
             if (_currentHandHolding == hand)
             {
                 IsGrabbed = false;
+                _grabbedVec = Vector3.positiveInfinity;
                 _currentHandHolding = RhinoxHand.Invalid;
             }
         }
 
         private void Update()
         {
-            if (IsGrabbed)
+            if (IsGrabbed && _interactingJoint != null)
             {
+                //var proj = _interactingJoint.JointPosition.ProjectOnPlaneAndTranslate(transform.position, -transform.forward);
+                //var smrse = Vector3.ProjectOnPlane(_interactingJoint.JointPosition, -transform.forward);
+                float dAngle = Vector3.SignedAngle(_grabbedVec, _interactingJoint.JointPosition, -transform.forward);
 
+                //if (Mathf.Abs(dAngle) < .5f)
+                //return;
+
+                _currentValveRotation -= dAngle;
+                if (_currentValveRotation >= _fullyClosedAngle)
+                {
+                    _currentValveRotation = _fullyClosedAngle;
+                    OnFullyClosed?.Invoke(this);
+                }
+                else if (_currentValveRotation <= _fullyOpenAngle)
+                {
+                    _currentValveRotation = _fullyOpenAngle;
+                    OnFullyOpen?.Invoke(this);
+                }
+                _grabbedVec = _interactingJoint.JointPosition;
+
+
+                OnValueUpdate?.Invoke(this, _currentValveRotation);
+
+                //ROTATE VISUAL
+                _visualTransform.localEulerAngles = _visualTransform.localEulerAngles.With(null, null, _currentValveRotation);
+                // (0f,0f,0f,Space.Self);
+                //_visualTransform.localEulerAngles.With(null,null,)
             }
         }
 
@@ -237,6 +249,16 @@ namespace Rhinox.XR.Grapple.It
                     GizmoExtensions.DrawSolidDonut(trans.position, trans.right, -trans.forward,
                         _minRadius, _maxRadius);
                 }
+            }
+        }
+
+        private void Reset()
+        {
+            if (_visualTransform == null)
+            {
+                var go = new GameObject("Visual");
+                _visualTransform = go.transform;
+                _visualTransform.parent = transform;
             }
         }
 #endif
